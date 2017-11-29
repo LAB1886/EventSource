a9fc0d1482deeb72fe7fa609c03254e2186a1c3c
Mathias Köhnke <mathias.koehnke@daimler-x.com>
2017-11-29 11:41:20 +0100

    Fixed license information

--- Framework/EventSource.h
+++ Framework/EventSource.h
@@ -1,19 +1,19 @@
 //
-//  SSE.h
-//  SSE
+//  EventSource.h
+//  EventSource
 //
-//  Created by Mathias Köhnke on 29.11.17.
-//  Copyright © 2017 Inaka. All rights reserved.
+//  Created by Andres on 2/13/15.
+//  Copyright (c) 2015 Inaka. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for SSE.
-FOUNDATION_EXPORT double SSEVersionNumber;
+//! Project version number for EventSource.
+FOUNDATION_EXPORT double EventSourceVersionNumber;
 
-//! Project version string for SSE.
-FOUNDATION_EXPORT const unsigned char SSEVersionString[];
+//! Project version string for EventSource.
+FOUNDATION_EXPORT const unsigned char EventSourceVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SSE/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <EventSource/PublicHeader.h>
 
 
