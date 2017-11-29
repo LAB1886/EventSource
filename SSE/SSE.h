6c58dfe95e0496d1fca81de75d5037e8f1b72a51
Mathias Köhnke <mathias.koehnke@daimler-x.com>
2017-11-29 11:10:39 +0100

    Added carthage support

---
+++ SSE/SSE.h
@@ -0,0 +1,19 @@
+//
+//  SSE.h
+//  SSE
+//
+//  Created by Mathias Köhnke on 29.11.17.
+//  Copyright © 2017 Inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSE.
+FOUNDATION_EXPORT double SSEVersionNumber;
+
+//! Project version string for SSE.
+FOUNDATION_EXPORT const unsigned char SSEVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSE/PublicHeader.h>
+
+
